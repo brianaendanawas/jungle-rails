3fe07b98ec681b2d5a9a3cdc191ac52759ea44ef
Briana Endanawas <brianamay48@gmail.com>
2022-04-18 06:54:30 -0400

    create “new” action for Admin::CategoriesController

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,12 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new
+  end
 end
