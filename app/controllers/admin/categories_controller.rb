91c154aa6e9b4a99856f9d4ec720ab6a2655607b
Briana Endanawas <brianamay48@gmail.com>
2022-04-18 07:12:13 -0400

    finish categories controller for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
@@ -8,6 +8,20 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
   end
 end
