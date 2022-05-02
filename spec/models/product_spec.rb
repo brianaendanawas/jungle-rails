5cadb46a06ccd3d9f74b6e1be817790644c9a4e0
Briana Endanawas <brianamay48@gmail.com>
2022-05-02 06:27:42 -0400

    create model specs for the Product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "product will save with all four fields" do
+      category = Category.create(name: "Food")
+      product = Product.create(name: "Apple", price: 10, quantity: 5, category: category)
+      expect(product.errors.full_messages).to be_empty
+    end
+
+    it "is not valid without a name" do
+      category = Category.create(name: "Food")
+      product = Product.create(name: nil, price: 10, quantity: 5, category: category)
+      expect(product.errors.full_messages).not_to be_empty
+    end
+
+    it "is not valid without a price" do
+      category = Category.create(name: "Food")
+      product = Product.create(name: "Apple", quantity: 5, category: category)
+      expect(product.errors.full_messages).not_to be_empty
+    end
+
+    it "is not valid without a quantity" do
+      category = Category.create(name: "Food")
+      product = Product.create(name: "Apple", price: 10, quantity: nil, category: category)
+      expect(product.errors.full_messages).not_to be_empty
+    end
+
+    it "is not valid without a category" do
+      category = Category.create(name: "Food")
+      product = Product.create(name: "Apple", price: 10, quantity: 5, category: nil)
+      expect(product.errors.full_messages).not_to be_empty
+    end
+  end
+end
