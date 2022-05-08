7dc3c139e18be8be25de48af51f3104d34f1c226
Briana Endanawas <brianamay48@gmail.com>
2022-05-08 02:31:21 -0400

    finish defining validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
     it "is valid with all attributes" do
-      user = User.new(first_name: "Briana", last_name: "May", email: "briana@gmail.com", password: "123", password_confirmation: "123")
-      expect(user.errors.full_messages).to be_empty
+      user = User.create(first_name: "Briana", last_name: "May", email: "briana@gmail.com", password: "123", password_confirmation: "123")
+      expect(user).to be_valid
+    end
+
+    it "is valid when password and password_confirmation match" do
+      user = User.create(first_name: "Briana", last_name: "May", email: "briana@gmail.com", password: "123", password_confirmation: "1234")
+      expect(user).to_not be_valid
+    end
+
+    it "is invalid without unique email" do
+      user1 = User.create(first_name: "Briana", last_name: "May", email: "briana@gmail.com", password: "123", password_confirmation: "1234")
+      user2 = User.create(first_name: "Briana", last_name: "May", email: "BRIANA@gmail.com", password: "123", password_confirmation: "1234")
+      expect(user2).to_not be_valid
     end
   end
 end
