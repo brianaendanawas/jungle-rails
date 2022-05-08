6a64d2e00a0e4ecd9c3f8a0c5636a786d076e07a
Briana Endanawas <brianamay48@gmail.com>
2022-05-08 05:43:01 -0400

    password minimum length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,10 @@ RSpec.describe User, type: :model do
       user2 = User.create(first_name: "Briana", last_name: "May", email: "BRIANA@gmail.com", password: "123", password_confirmation: "1234")
       expect(user2).to_not be_valid
     end
+
+    it "is invalid with a password less than 3 characters" do
+      user = User.create(first_name: "Briana", last_name: "May", email: "briana@gmail.com", password: "12", password_confirmation: "12")
+      expect(user).to_not be_valid
+    end
   end
 end
